f58fa999bae71a308be556bbe4ec2f781662274c
amityadav341 <rockamit341@gmail.com>
2021-06-02 12:58:33 +0530

    Updates

---
+++ precache-manifest.2cb008502527ae35b398506f9ae9aa63.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b8dc42827dd21b870e07bd9107dbc5",
+    "url": "/MyportfolioAmitYadav/index.html"
+  },
+  {
+    "revision": "6298921cb0c5a338bc05",
+    "url": "/MyportfolioAmitYadav/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0980b31e773c5a0e67fe",
+    "url": "/MyportfolioAmitYadav/static/css/main.86f3ee3f.chunk.css"
+  },
+  {
+    "revision": "6298921cb0c5a338bc05",
+    "url": "/MyportfolioAmitYadav/static/js/2.8c9e835c.chunk.js"
+  },
+  {
+    "revision": "c8ffd5abc06d09fa1ebb018a30d7c2c6",
+    "url": "/MyportfolioAmitYadav/static/js/2.8c9e835c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0980b31e773c5a0e67fe",
+    "url": "/MyportfolioAmitYadav/static/js/main.4f107f9d.chunk.js"
+  },
+  {
+    "revision": "5d5e19d032bc3d5c7de4",
+    "url": "/MyportfolioAmitYadav/static/js/runtime-main.ea236434.js"
+  },
+  {
+    "revision": "98fcc06df99bd61b8139da40ea42664d",
+    "url": "/MyportfolioAmitYadav/static/media/Ml.98fcc06d.JPG"
+  },
+  {
+    "revision": "2dc56d9a62c89216e646d41570159965",
+    "url": "/MyportfolioAmitYadav/static/media/amit.2dc56d9a.jpeg"
+  },
+  {
+    "revision": "9a4f008d49e9b0fbc88c313ec8aa7b49",
+    "url": "/MyportfolioAmitYadav/static/media/amit.9a4f008d.pdf"
+  },
+  {
+    "revision": "ed1d518ed00f4049578ecc1f4b3ba403",
+    "url": "/MyportfolioAmitYadav/static/media/covid.ed1d518e.JPG"
+  },
+  {
+    "revision": "d3b03fed134b4555028bffdd69d62594",
+    "url": "/MyportfolioAmitYadav/static/media/home.d3b03fed.jpg"
+  },
+  {
+    "revision": "82d4742da9d7108008923bcda31a82ba",
+    "url": "/MyportfolioAmitYadav/static/media/portfolio.82d4742d.JPG"
+  }
+]);
